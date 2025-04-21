7e5f52ec699e5464b556810c8b6a637f6f4768a3
SyedFarzeenAhmad <syedfarzeen18@gmail.com>
2025-04-21 15:16:07 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // This is important for production paths!
   build: {
-    outDir: 'dist', 
-    assetsDir: 'assets', 
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         format: 'es', // ES Modules
