783ab565d9f5a5dcc1f22920d45e0419db69b573
SyedFarzeenAhmad <syedfarzeen18@gmail.com>
2025-04-21 13:57:01 +0530

    Tech component done0

--- src/constants/index.js
+++ src/constants/index.js
@@ -73,10 +73,6 @@ import {
       icon: javascript,
     },
     {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
       name: "React JS",
       icon: reactjs,
     },
@@ -93,10 +89,6 @@ import {
       icon: nodejs,
     },
     {
-      name: "MongoDB",
-      icon: mongodb,
-    },
-    {
       name: "Three JS",
       icon: threejs,
     },
@@ -107,10 +99,6 @@ import {
     {
       name: "figma",
       icon: figma,
-    },
-    {
-      name: "docker",
-      icon: docker,
     },
   ];
   
